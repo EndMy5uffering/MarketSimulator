c779bd73549ad5df39c618d351d2a2aa53d78edd
Badading <badading.mc@gmail.com>
2021-04-29 01:50:50 +0200

    Base functionality for com.market.User

--- src/com/market/account/User.java
+++ src/com/market/account/User.java
@@ -1,5 +1,53 @@
 package com.market.account;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+    private final String uuid;
+    private String userName;
+    private List<Account> accounts= new ArrayList<>();
+
+    public User(String uuid, String userName) {
+        this.uuid = uuid;
+        this.userName = userName;
+    }
+
+    public boolean deleteUser() {
+        boolean temp = true;
+        for(Account a: this.accounts) {
+            temp = temp && removeFromAccount(a);
+        }
+        return temp;
+    }
+
+    public boolean addToAccount(Account account) {
+        if(account == null)
+            return false;
+        //TODO: Call function in Account obj to add user
+        this.accounts.add(account);
+        return true;
+    }
+
+    public boolean removeFromAccount(Account account) {
+        if(account == null)
+            return false;
+        //TODO: Call function in Account obj to remove user
+        this.accounts.remove(account);
+        return true;
+
+
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 }
--- src/com/market/main/MainMarket.java
+++ src/com/market/main/MainMarket.java
@@ -3,7 +3,7 @@ package com.market.main;
 public class MainMarket {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World");
 	}
 	
 }
